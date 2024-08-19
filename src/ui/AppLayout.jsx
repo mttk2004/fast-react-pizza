79d457d25892c282a767a478143cb4da17426371
= <=>
2024-08-19 16:44:26 +0700

    v2.0: updated

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,9 @@
+/**
+ *  Project: fast-react-pizza
+ *  File: AppLayout.jsx
+ *  Created: 2:58 CH, 19/08/2024
+ *  Author: Mai Tran Tuan Kiet
+ *  "Family is where life begins and love never ends."
+ */
+
+
---
+++ src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+/**
+ *  Project: fast-react-pizza
+ *  File: Loader.jsx
+ *  Created: 4:26 CH, 19/08/2024
+ *  Author: Mai Tran Tuan Kiet
+ *  "Family is where life begins and love never ends."
+ */
+
+

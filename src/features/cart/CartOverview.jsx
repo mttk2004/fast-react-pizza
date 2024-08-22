45f94f32262a52319e59da1e7d1b786546ac2281
= <=>
2024-08-22 16:36:48 +0700

    v8.0: updated

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div className="flex items-center justify-between bg-stone-700 px-3 py-2 uppercase text-yellow-50 sm:px-4 sm:py-3 md:px-6 md:py-4">
+    <div className="flex items-center justify-between bg-stone-700 px-3 py-1.5 uppercase text-yellow-50 sm:px-4 sm:py-2 md:px-6 md:py-3">
       <p className="space-x-3">
         <span>23 pizzas</span>
         <span>$23.45</span>
